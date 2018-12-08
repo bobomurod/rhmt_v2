530eeef35109764d1d7b6d28de754cc6b44c04d5
bobomurod <bobomurod.boboev@yandex.ru>
2018-12-08 13:07:40 +0500

    moving to other workplace

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,14 @@
+const initmodel = require("../models/initv1.model");
+const testmodel = require("../models/test.model");
 var appRouter = function(app) {
   app.get("/test", function(req, res, next) {
     console.log("Responding...");
-    res.status(200).json({ message: "This is test" });
+    res.status(200).json(test_cont);
     console.log("Done.");
+  });
+
+  app.get("/init_v1", function(req, res, next) {
+    res.status(200).json("init");
   });
 };
 
